d32062b8e6ef8e8cd3b5da68142c8b4b296c8a73
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2023-06-01 14:50:13 +0300

    тестовое поле

--- project/Program.cs
+++ project/Program.cs
@@ -1,30 +1,77 @@
 ﻿using model;
-using System;
+using System.Xml.Linq;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+
 
 namespace ConsoleApp1
 {
-    internal class Program
+    public class Program
     {
-        private static TelegramBotClient client=null!;
         private static string token { get; set; } = "5926149406:AAG24W75qBUKg-IiGcjPYXRb9np43H7bEBU";
+        private static TelegramBotClient client = new TelegramBotClient(token);
 
-         static void Main(string[] args)
+        static void Main(string[] args)
         {
-            client = new TelegramBotClient(token);
-            List<int> chat_id = new List<int>() { 2028880831, 450249503 };
-            foreach(int s in chat_id)
+            //List<int> chat_id = new List<int>() { 2028880831, 450249503 };
+            int chat = 2028880831;
+            //mes(chat, "дарова отец");
+            //Console.WriteLine(IsExist("sdf", "up"));
+            string Name = "df";
+            string Password = "gf";
+            using (ApplicationContext db = new ApplicationContext())
             {
-                mes(s);
+                var UserList = db.Users.Where(x => x.name == Name).Where(x => x.password == Password).ToList();                
+                if (UserList.Count == 0)
+                {
+                    Console.WriteLine( "ничего нет");
+                }
+                else
+                {
+                    foreach (var user in UserList)
+                    {
+                        Console.WriteLine(user.name);
+                    }
+                }
             }
+
+
+            var t = new List<string>() { "негр", "pizza", "upd" };
+
             Console.ReadLine();
 
         }
-        async static void mes(int chat_id)
+        async static void mes(int chat_id, string message)
         {
-            await client.SendTextMessageAsync(chat_id, "на экзамене халявы не будет");
+            await client.SendTextMessageAsync(chat_id, message);
+        }
+        public static string RegisterUser(string Name, string Password, string tglt, long chat)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                if (IsExist(Name, Password))
+                {
+                    string tgl = "@" + tglt;
+                    long tgid = chat;
+                    var s = new ChatUser { name = Name, password = Password, chat_id = tgid, tglink = tgl };
+                    db.Users.Add(s);
+                    db.SaveChanges();
+                    return $"Вы успешно зарегистрировались как {Name}";
+                }
+                return "есть уже такой пользователь";
 
+            }
+        }
+        public static bool IsExist(string Name, string Password)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var UserList = db.Users.Where(x => x.name == Name ).ToList().Where(x=>x.password==Password);
+
+                return UserList != null;
+            }
         }
     }
 }
